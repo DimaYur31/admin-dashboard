76b7b4786b5967a4bb0fd7b57f47825225336bbe
DimaYur31 <d_yurchik@bk.ru>
2023-12-24 23:27:54 +0300

    new user and new product pages

---
+++ app/dashboard/products/add/page.tsx
@@ -0,0 +1,31 @@
+
+import styles from "@/app/ui/dashboard/products/addProducts/addProducts.module.css";
+
+const AddProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <form>
+        <input type="text" placeholder="title" name="title" required/>
+        <select name="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="price" name="price" required/>
+        <input type="number" placeholder="stock" name="stock" required/>
+        <input type="text" placeholder="color" name="color"/>
+        <input type="text" placeholder="size" name="size"/>
+        <textarea
+          required
+          name="desc"
+          id="desc"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductPage;
